029fc6f1fc4c743acd37fc96176a7becb67cd54a
Omkar Birade <omkarbirade20@gmail.com>
2023-06-14 16:52:31 +0200

    test: mock stepsTracker module using regular mock

--- index.test.js
+++ index.test.js
@@ -1,8 +1,23 @@
-import { getHealthSummary } from "./index.js";
+import { getHealthSummary } from ".";
+import { jest } from "@jest/globals";
+
+const mockDailySteps = [
+  6_453, 6_987, 9_876, 11_897, 12_549, 14_895, 5_765, 18_920, 8_714, 9_007,
+];
+const mockGetDailySteps = jest.fn();
 
 describe("Health summary", () => {
-  test("should return healthy summary for a single day", async () => {
+  beforeAll(() => {
+    jest.mock("./stepsTracker", () => ({
+      getDailySteps: mockGetDailySteps.mockImplementation((numberOfDays) =>
+        mockDailySteps.slice(0, numberOfDays)
+      ),
+    }));
+  });
+
+  afterAll(jest.clearAllMocks);
+  test("should return healthy summary for a single day", () => {
     const result = getHealthSummary(1);
-    expect(result).toMatchObject({ numberOfDays: 1, totalSteps: 9_000 });
+    expect(result).toMatchObject({ numberOfDays: 1, totalSteps: 6_453 });
   });
 });
